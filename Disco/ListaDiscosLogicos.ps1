ea00eff568b4502629eba65076bca9d2189c64f8
Eduardo Rodrigues da Silva <83621137+duli0810@users.noreply.github.com>
2021-05-05 12:57:14 -0300

    Add files via upload

---
+++ Disco/ListaDiscosLogicos.ps1
@@ -0,0 +1,24 @@
+﻿echo ""
+echo                                                "################ Lista Discos Logicos  ################"
+echo ""
+echo                                                  "################ Autor: EDUARDO RODRIGUES ################"
+echo ""
+echo                                                   "################ Criado em: 05/05/2021 ################"
+echo ""
+echo ""
+echo ""
+
+<#
+
+💻💻💻 Acesse o Blog e veja o passo a passo: https://acesso8.blogspot.com/ 💻💻💻
+
+O script Informa os Discos Logicos 
+
+OBS.: Os Discos Logicos são do  TIPO 3 ( Fisicos )
+
+#>
+
+###################################################### Lista Discos Logicos  ##############################################################
+
+
+Get-WmiObject Win32_logicaldisk -Filter "DriveType = 3" | ft 
