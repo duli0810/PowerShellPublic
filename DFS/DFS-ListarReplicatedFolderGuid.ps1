16331732ffbf10dc0c94235b7a70010ccdbe4ca4
Eduardo Rodrigues da Silva <83621137+duli0810@users.noreply.github.com>
2021-05-07 22:05:47 -0300

    Update DFS-ListarReplicatedFolderGuid.ps1

--- DFS/DFS-ListarReplicatedFolderGuid.ps1
+++ DFS/DFS-ListarReplicatedFolderGuid.ps1
@@ -8,10 +8,12 @@ echo ""
 echo ""
 <#
 
+💻💻💻 Acesse o Blog e veja o passo a passo: https://acesso8.blogspot.com/ 💻💻💻
+
 Esse comando irá listar o ID da pasta replicada bem como o seu nome conforme pode ser visto na imagem abaixo; 
 
 Ref.: https://docs.microsoft.com/en-us/archive/blogs/askds/manually-clearing-the-conflictanddeleted-folder-in-dfsr
-      http://williansco.blogspot.com/2016/10/pasta-conflictanddeleted-excedeu-seu.html
+      
 #>
 
 WMIC.EXE /namespace:\\root\microsoftdfs path dfsrreplicatedfolderconfig get replicatedfolderguid,replicatedfoldername
