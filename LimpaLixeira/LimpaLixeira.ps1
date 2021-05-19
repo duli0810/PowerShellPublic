e89c01433c570de27a1df734d6a2a49419005ac6
Eduardo Rodrigues da Silva <83621137+duli0810@users.noreply.github.com>
2021-05-19 17:09:35 -0300

    Add files via upload

---
+++ LimpaLixeira/LimpaLixeira.ps1
@@ -0,0 +1,24 @@
+echo ""
+echo                                              "################ Script - Limpa a Lixeira ################"
+echo ""
+echo                                                  "################ Autor: EDUARDO RODRIGUES ################"
+echo ""
+echo                                                   "################ Criado em: 14/04/2021 ################"
+echo ""
+echo ""
+echo ""
+
+<#
+
+O Comando abaixo Limpa a Lixeira; 
+
+
+#>
+############################################################ LIMPANDO LIXEIRA ###########################################################################
+
+Clear-RecycleBin -force
+
+
+
+
+
