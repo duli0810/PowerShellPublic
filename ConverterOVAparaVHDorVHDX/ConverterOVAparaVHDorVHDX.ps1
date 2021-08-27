d2f91c7e4bf6b50bb6ed38f631bdca5b252fd44f
Eduardo Rodrigues da Silva <edu0810@gmail.com>
2021-08-27 12:23:25 -0300

    Add files via upload

---
+++ ConverterOVAparaVHDorVHDX/ConverterOVAparaVHDorVHDX.ps1
@@ -0,0 +1,65 @@
+﻿echo ""
+echo                                              "################ Converter .ova para .vhd / vhdx ################"
+echo ""
+echo                                                  "################ Autor: EDUARDO RODRIGUES ################"
+echo ""
+echo                                                   "################ Criado em: 20/08/2021 ################"
+echo ""
+echo ""
+echo ""
+###################################################################### INSTRUÇÃO ##############################################################################
+
+<#
+
+1- Extraia o .ova ( Use software como winrar ou .zip), será gerado dois arquivos o .vmdk e o .ovf; 
+2- Instale o Microsoft Virtual Machine Converter; 
+3- Edite as Path do script "ConverterOVAparaVHDorVHDX.ps1" , com a origem (VMDK) e destino (VDH / VHDX);
+4- Execute o script "ConverterOVAparaVHDorVHDX.ps1";
+5- Será gerado a VHD ou VHDX no diretorio selecionado; 
+
+#>
+
+write-host -ForegroundColor yellow  "ATENÇÃO: Caso apresente um erro semelhante ao exemplo abaixo: "
+echo ""
+write-host -ForegroundColor red  'ConvertTo-MvmcVirtualHardDisk : The entry 00000000-0000-0000-0000-000000000000 is not a supported disk database entry for the descriptor.'
+echo ""
+write-host -ForegroundColor yellow  'Segue link com correção -> endereço_do_github!!!'
+
+############################################################ Declarando Varialvel ##############################################################################
+
+$MVMC     = "C:\Program Files\Microsoft Virtual Machine Converter\MvmcCmdlet.psd1" #Diretorio Default
+
+############################################################## Informe os Path #################################################################################
+
+$DirVMDK  = "<Informe a path da VMDK>"                                 # Informe a path da VMDK juntamente com o nome do disco ( Ex.: C:\disco.vmdk )
+$DirVHD   = "<Informe a path onde será salvo a VHD ou VHDX>"           # Informe a path onde será salvo a VHD ou VHDX ( Ex.: C:\ )
+
+############################################################ Selecione o Tipo e Formato da VHD ##################################################################
+<#
+
+OBS: Alteração opcional, o script esta como padrao DynamicHardDisk e vhdx
+
+#>
+<#
+
+**** VHD Type *** 
+
+DynamicHardDisk
+FixedHardDisk
+
+#>
+<#
+
+**** VHD Format *** 
+
+vhd
+vhdx
+
+#>
+
+############################################################ Converter .ova para .vhd / vhdx ######################################################################
+
+Import-Module $MVMC
+
+ConvertTo-MvmcVirtualHardDisk -SourceLiteralPath "$DirVMDK" -VhdType DynamicHardDisk -VhdFormat vhdx -destination $DirVHD 
+
---
+++ ConverterOVAparaVHDorVHDX/README.md
@@ -0,0 +1,28 @@
+# Criptografia de Dados Transparente (TDE)
+
+
+O passo a passo irá mostrar como criptografar no modo TDE 
+
+
+" ⚠️Atenção: TDE (Transparent Data Encryption - Criptografia de Dados Transparente)
+
+O TDE proteje através da criptgrafia o banco de dados por completo.
+
+A criptografia de um arquivo de banco de dados é feita no nível da página. As páginas em um banco de dados criptografado são criptografadas antes de serem gravadas no disco e descriptografadas quando lidas na memória. A TDE não aumenta o tamanho do banco de dados criptografado. "
+
+#
+ ⭐⭐⭐ Não possuo quaisquer responsabilidade na execução dos procedimento. ⭐⭐⭐
+
+#
+
+
+#
+🖥️ Acesse o Blog e veja o passo a passo: https://acesso8.blogspot.com/
+
+#
+⭐Referencia:
+
+"Criptografia de Dados Transparente (TDE)"
+
+    https://docs.microsoft.com/pt-br/sql/relational-databases/security/encryption/transparent-data-encryption?view=sql-server-ver15
+
